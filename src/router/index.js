d607925d804db31dbc41419dc0a8fab49a4ee86a
Vukasin Macura <moj_email@gmail.com>
2020-10-02 20:33:09 +0200

    1. zadatak, kreiranje aplikacije, kreiranje dve komponente AppCustomers i AppProducts i dodavanje dve rute

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import AppCustomers from '../components/AppCustomers.vue'
+import AppProducts from '../components/AppProducts.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/customers',
+    name: 'app-customers',
+    component: AppCustomers
+  },
+
+  {
+    path: '/products',
+    name: 'app-products',
+    component: AppProducts
+  },
+
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
